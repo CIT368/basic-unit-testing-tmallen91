10da75cf7e3248b16279081ee2a1c681047018bb
Travis Allen <Travis.Allen@westpharma.com>
2020-02-25 13:02:34 -0500

    Added more Unit Tests

--- TelephoneTest.java
+++ TelephoneTest.java
@@ -1,10 +1,31 @@
-import static org.junit.Assert.assertEquals;
+// Author:     Travis Allen
+// Date:       25FEB2020
+// Purpose:    Test cases for the Telephone.class format() method.
+
+import static org.junit.Assert.*;
+
 
 import org.junit.Test;
 
 public class TelephoneTest{
-    @Test
-    public void ChallengeX(){
+    @Test //Test expected user input
+    public void ChallengeNumbers(){
         assertEquals("(570)322-3128", Telephone.format("5703223128"));
     }
+
+    @Test //Challenge if input is characters
+    public void ChallengeChars(){
+        assertEquals("(abc)def-ghij", Telephone.format("abcdefghij"));
+    }
+
+    @Test //Test if the function does not format strings longer than the UNFORMATTED_LENGTH 
+    public void FailFormatting(){
+        assertEquals("12345678901", Telephone.format("12345678901"));
+    }
+
+    @Test //Something other than assertEquals. Challenge that the formatted string is three extra characters long.
+    public void ChallengeLength(){
+        assertTrue(Telephone.format("1234567890").length() == 13);
+    }
+
 }
